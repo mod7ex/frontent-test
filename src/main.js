abf6dd13d35983caebf0db7842887f9f2e3348fc
Mourad EL CADI <mouradface1998@gmail.com>
2022-02-08 10:22:16 +0100

    textarea fixed

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from "vue";
 import store from "./store";
 import App from "./App.vue";
 
-createApp(App).use(store).mount("#app");
+const app = createApp(App);
+
+app.use(store);
+
+app.mount("#app");
